62dd2875edb86d3c09b177c4d0bf1bb3353400a3
davimehra <davimehra1234@gmail.com>
2023-08-08 07:30:10 +0530

    seccond Commit

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/',(req,res,next)=>{
     res.status(200);
     res.json({message:"Get Request / response"})
 })
+app.get("/user/signin",(req,res,next)=>{
+    res.status(200);
+    res.json({userid:'122344',email:"davimehra1234@gmail.com"})
+})
 app.listen(process.env.PORT,()=>{
 console.log(`Listening at port ${process.env.PORT}`)
 })
